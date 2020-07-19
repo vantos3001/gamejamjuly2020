16ffbbdf4c01c704efd9fe65c5d0f0df32359d4c
vantos3001 <iw.lasgin2011@gmail.com>
2020-07-19 13:41:20 +0300

    destroy pickable object

--- Assets/Scripts/Game/PickableObjects/PickableObject.cs
+++ Assets/Scripts/Game/PickableObjects/PickableObject.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using UnityEngine;
 
 public enum PickableObjectType{
     Coin,
     Corvalol
 }
 
-public abstract class PickableObject : IPickableObject
+public abstract class PickableObject : MonoBehaviour, IPickableObject
 {
     protected abstract PickableObjectType Type { get;}
 
     public virtual void Pick()
     {
-        throw new NotImplementedException();
+        Destroy(gameObject);
     }
 }
