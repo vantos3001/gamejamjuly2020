608b15184abb072a7c47a9591130fcf96c483987
vantos3001 <iw.lasgin2011@gmail.com>
2020-07-19 09:51:31 +0300

    add health

--- Assets/Scripts/Game/Managers/EventManager.cs
+++ Assets/Scripts/Game/Managers/EventManager.cs
@@ -3,9 +3,16 @@
 public static class EventManager
 {
     public static Action<AbilityType> OnAbilityLevelUp;
+
+    public static Action<float> OnHealthChanged;
     
     public static void NotifyOnAbilityLevelUp(AbilityType abilityType)
     {
         OnAbilityLevelUp?.Invoke(abilityType);
+    }
+
+    public static void NotifyOnHealthChanged(float currentHealth)
+    {
+        OnHealthChanged?.Invoke(currentHealth);
     }
 }
---
+++ Assets/Scripts/Game/Player/Health.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private const float DEFAULT_DAMAGE_PER_SECOND = 1f;
+    
+    private float _currentHealth = 0;
+    private float _currentDamagePerSecond;
+
+    public Action HealthEnded;
+
+    private float CurrentHealth
+    {
+        get => _currentHealth;
+        set
+        {
+            EventManager.NotifyOnHealthChanged(_currentHealth);
+            _currentHealth = value;
+        }
+    }
+
+    private bool _isLive;
+
+    public void Init(float maxHealth)
+    {
+        _currentDamagePerSecond = DEFAULT_DAMAGE_PER_SECOND;
+        CurrentHealth = maxHealth;
+        _isLive = true;
+    }
+
+    public void AddHealth(float health)
+    {
+        CurrentHealth += health;
+        
+        if(CurrentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void FreezeDamagePerSecond()
+    {
+        _currentDamagePerSecond = 0;
+    }
+
+    public void UnfreezeDamagePerSecond()
+    {
+        _currentDamagePerSecond = DEFAULT_DAMAGE_PER_SECOND;
+    }
+    
+    private void Update()
+    {
+        if (_isLive)
+        {
+            CurrentHealth -= _currentDamagePerSecond * Time.deltaTime;
+            
+            if (_currentHealth <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+    
+    public void Kill()
+    {
+        CurrentHealth = 0;
+        _isLive = false;
+        HealthEnded?.Invoke();
+    }
+}
